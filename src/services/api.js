6fa24b3b85365b343d802205bb70956b6c10ee11
Tam Tran <tam.half28@gmail.com>
2026-05-08 22:33:28 +0700

    deploy test

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://34..47.160/api',
     headers: {
         'Content-Type': 'application/json',
     },
