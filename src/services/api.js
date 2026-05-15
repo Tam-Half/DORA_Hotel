6081f4201b1e4ce09d74f0df051adb07cc5eab82
quangkid2212 <163752892+hckq2212@users.noreply.github.com>
2026-05-15 17:36:51 +0700

    test fallback api url

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API_URL = window.__ENV__?.API_URL || "http://localhost:3000";
 
 const api = axios.create({
     //   baseURL: `${API_URL}/api`,
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || `${API_URL}/api`,
     headers: {
         "Content-Type": "application/json",
     },
